2fc0e7eef9d92117184afaccfb124c45fad42b39
Ehsan Ghasemi <ehsanghasemi7998@gmail.com>
2024-11-19 12:59:58 +0330

    Printing 2d vectors has been added.

--- include/vector.h
+++ include/vector.h
@@ -3,6 +3,7 @@
 #include <vector>
 #include <cstdint>
 #include <numeric>
+#include <iomanip>
 
 namespace vec {
 
@@ -10,11 +11,30 @@ namespace vec {
   void print(std::vector<T> input, std::string name="-") {
     std::cout << name << ": ["; 
     for (uint32_t i = 0; i < input.size(); ++i) { 
-      std::cout << input[i];
+      std::cout << std::fixed << std::setprecision(8) << input[i];
       if (i < input.size() - 1) { 
         std::cout << ", "; 
       } 
     }
+    std::cout << "]" << std::endl; 
+  } // void print
+
+  template<typename T>
+  void print(std::vector<std::vector<T>> input, std::string name="-") {
+    std::cout << name << ": ["; 
+    for (uint32_t i = 0; i < input.size(); ++i) { 
+      std::cout << "["; 
+      for (uint32_t j = 0; j < input[i].size(); ++j) { 
+        std::cout << input[i][j]; 
+        if (j < input[i].size() - 1) { 
+          std::cout << ", "; 
+        }
+      } // iterate over j
+      std::cout << "]"; 
+      if (i < input.size() - 1) { 
+        std::cout << std::endl; 
+      }
+    } // iterate over i
     std::cout << "]" << std::endl; 
   } // void print
 
